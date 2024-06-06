41ce04b5d0736da9059bcd775e008fcee36b9691
abdirazack <abdirizakomar254@gmail.com>
2024-06-06 13:11:54 +0300

    refactor: Implement MustVerifyEmail interface in OrganizationUser model

--- app/Models/OrganizationUser.php
+++ app/Models/OrganizationUser.php
@@ -13,8 +13,9 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 
-class OrganizationUser extends Authenticatable
+class OrganizationUser extends Authenticatable implements MustVerifyEmail
 {
     use HasFactory, Notifiable, SoftDeletes, HasRoles;
 
