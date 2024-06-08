7e29e39c0ecf48d0044fbe14aa9033f5a9059396
abdirazack <abdirizakomar254@gmail.com>
2024-06-08 09:12:24 +0300

    chore: Update npm dependencies and add Alpine.js for improved interactivity

--- resources/views/livewire/organization-user/create.blade.php
+++ resources/views/livewire/organization-user/create.blade.php
@@ -2,7 +2,7 @@
     {{-- Create User form  --}}
     <div class="flex justify-between m-4">
         <h1 class="text-xl font-bold">Create User</h1>
-         <button wire:click="$dispatch('close')" class="btn btn-sm btn-circle btn-ghost ">✕</button>
+        <button wire:click="$dispatch('close')" class="btn btn-sm btn-circle btn-ghost ">✕</button>
     </div>
     <div>
         @if (session()->has('message'))
@@ -37,9 +37,17 @@
                     </div>
                 </div>
 
-                <div>
-                    <input placeholder="Password" wire:model="password" id="password" type="password"
-                        class="input input-bordered w-full max-w-xs" />
+
+                <div x-data="{ show: false }">
+                    <div class="relative">
+                        <input placeholder="Password" wire:model="password" id="password"
+                            :type="show ? 'text' : 'password'" class="input input-bordered w-full max-w-xs pr-10" />
+                        <button type="button" @click="show = !show"
+                            class="absolute inset-y-0 right-0 px-3 flex items-center text-gray-500 focus:outline-none">
+                            <i x-show="!show" class="fas fa-eye"></i>
+                            <i x-show="show" class="fas fa-eye-slash"></i>
+                        </button>
+                    </div>
                     <div>
                         @error('password')
                             <span class="text-red-500">{{ $message }}</span>
@@ -47,11 +55,17 @@
                     </div>
                 </div>
 
-
-                <div>
-                    <input placeholder="confirm Password" wire:model="password_confirmation" id="password_confirmation"
-                        type="password" class="input input-bordered w-full max-w-xs" />
-
+                <div x-data="{ show: false }">
+                    <div class="relative">
+                        <input placeholder="Confirm Password" wire:model="password_confirmation"
+                            id="password_confirmation" :type="show ? 'text' : 'password'"
+                            class="input input-bordered w-full max-w-xs pr-10" />
+                        <button type="button" @click="show = !show"
+                            class="absolute inset-y-0 right-0 px-3 flex items-center text-gray-500 focus:outline-none">
+                            <i x-show="!show" class="fas fa-eye"></i>
+                            <i x-show="show" class="fas fa-eye-slash"></i>
+                        </button>
+                    </div>
                     <div>
                         @error('password_confirmation')
                             <span class="text-red-500">{{ $message }}</span>
@@ -59,6 +73,7 @@
                     </div>
                 </div>
             </div>
+
 
 
 
